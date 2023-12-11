bc70915050b98b26d029f89725036898b57f61c6
Herbertgabs <herbertgacruz@gmail.com>
2023-12-11 15:52:11 -0300

    a
    aa

--- trilha-net-poo-desafio/Program.cs
+++ trilha-net-poo-desafio/Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "1 pro", imei: "123111", memoria: 32);
+nokia.ligar();
+nokia.InstalarAplicativo("Wild Rift");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:"):
+Smartphone iphone = new Iphone(numero: "5323", modelo: "8 plus", imei: "1144111", memoria: 128);
+nokia.ligar();
+nokia.InstalarAplicativo("TFT");
